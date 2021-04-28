08b3e68d7430a493e329472acfeb092231b62089
Iustina Filipescu <62960885+iustinafilipescu@users.noreply.github.com>
2021-04-28 12:46:12 +0300

    Add files via upload

---
+++ DiscordBot/src/main/java/Bot.java
@@ -0,0 +1,14 @@
+import events.HelloEvent;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Bot {
+    public static void main(String[] args) throws LoginException {
+        JDA jda =  JDABuilder.createDefault("ODM2ODkzMTkxODQ3NjczOTMy.YIknhQ.9_WoCxhmu0nT3-JNTDDi-dWPQks").build();
+
+        jda.addEventListener(new HelloEvent());
+
+    }
+}
---
+++ DiscordBot/src/main/java/events/HelloEvent.java
@@ -0,0 +1,15 @@
+package events;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class HelloEvent extends ListenerAdapter {
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event)
+    {
+        String messageSent=event.getMessage().getContentRaw();
+        if(messageSent.equalsIgnoreCase("hello") && !event.getMember().getUser().isBot())
+        {
+            event.getChannel().sendMessage("Hello "+ event.getMember().getUser().getName()+" !").queue();
+        }
+    }
+}
