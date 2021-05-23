713d78dceaa46bca14b82ad072faeb487421af55
Iustina Filipescu <62960885+iustinafilipescu@users.noreply.github.com>
2021-05-23 15:14:15 +0300

    Add files via upload

--- DiscordBot/src/main/java/Bot.java
+++ DiscordBot/src/main/java/Bot.java
@@ -1,9 +1,7 @@
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
-import commands.HelloCommand;
-import commands.ImageCommand;
-import commands.InviteCommand;
-import commands.ServerInfo;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import commands.*;
 import events.UserInfoCommand;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -16,19 +14,23 @@ public class Bot {
 
 
         jda.addEventListener(new UserInfoCommand());
+        jda.addEventListener(new MemeCommand());
+
+        EventWaiter waiter = new EventWaiter();
 
         CommandClientBuilder builder = new CommandClientBuilder();
        builder.setOwnerId("836893191847673932");
         builder.setPrefix("-");
         builder.setHelpWord("helpme");
         builder.addCommand(new ServerInfo());
-        builder.addCommand(new HelloCommand());
+        builder.addCommand(new HelloCommand(waiter));
         builder.addCommand(new InviteCommand());
         builder.addCommand(new ImageCommand());
+        builder.addCommand(new UserInfo(waiter));
 
         CommandClient client =builder.build();
 
         jda.addEventListener(client);
-
+        jda.addEventListener(waiter);
     }
 }
--- DiscordBot/src/main/java/commands/HelloCommand.java
+++ DiscordBot/src/main/java/commands/HelloCommand.java
@@ -1,21 +1,38 @@
 package commands;
 
+
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
-
-public class HelloCommand extends Command {
-
-    public HelloCommand() {
-
-        this.name="hello";
-
-        this.help="Says hello to the user";
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import java.util.concurrent.TimeUnit;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+public class HelloCommand extends Command
+{
+    private final EventWaiter waiter;
+    public HelloCommand(EventWaiter waiter)
+    {
+        this.waiter = waiter;
+        this.name = "hello";
+        this.aliases = new String[]{"hi"};
+        this.help = "says hello and waits for a response";
     }
 
     @Override
-    protected void execute(CommandEvent commandEvent) {
-        if (commandEvent.getArgs().equalsIgnoreCase("")) {
-            commandEvent.reply("Hello " + commandEvent.getMember().getUser().getName() + " !");
-        }
+    protected void execute(CommandEvent event)
+    {
+        // ask what the user's name is
+        event.reply("Hello. What is your name?");
+
+        // wait for a response
+        waiter.waitForEvent(MessageReceivedEvent.class,
+                // make sure it's by the same user, and in the same channel, and for safety, a different message
+                e -> e.getAuthor().equals(event.getAuthor())
+                        && e.getChannel().equals(event.getChannel())
+                        && !e.getMessage().equals(event.getMessage()),
+                // respond, inserting the name they listed into the response
+                e -> event.reply("Hello, `"+e.getMessage().getContentRaw()+"`! I'm `"+e.getJDA().getSelfUser().getName()+"`!"),
+                // if the user takes more than a minute, time out
+                1, TimeUnit.MINUTES, () -> event.reply("Sorry, you took too long."));
     }
+
 }
