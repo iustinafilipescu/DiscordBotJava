662c18bffa8115ddc00c3a42a255ef007d0aebeb
rares01 <79113662+rares01@users.noreply.github.com>
2021-06-01 13:28:09 +0300

    Update GuildMusicManager.java

--- DiscordBot/src/main/java/music/GuildMusicManager.java
+++ DiscordBot/src/main/java/music/GuildMusicManager.java
@@ -15,7 +15,6 @@ public class GuildMusicManager {
 
     /**
      * Creates a player and a track scheduler.
-     * @param manager Audio player manager to use for creating the player.
      */
     public GuildMusicManager(AudioPlayerManager manager) {
         player = manager.createPlayer();
@@ -24,7 +23,7 @@ public class GuildMusicManager {
     }
 
     /**
-     * @return Wrapper around AudioPlayer to use it as an AudioSendHandler.
+     Wrapper around AudioPlayer to use it as an AudioSendHandler.
      */
     public AudioPlayerSendHandler getSendHandler() {
         return new AudioPlayerSendHandler(player);
